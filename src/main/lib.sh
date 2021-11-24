ee2672d20df0cc057a2be3c88b4b75b78d735284
Dave Pointon FIAP MBCS <pointo1d@gmail.com>
2021-11-24 15:50:56 +0000

    Added the core include file - src/main/lib.sh

---
+++ src/main/lib.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# vim: et ai sw=2 sts=2
+################################################################################
+# File:         lib.sh
+# Description:  Betteredge specific test library routines - as with Betteredge
+#               itself, a test campaign comprises one, or more, test cases, each
+#               of which comprises one, or more, inidivdual tests. Note
+#               that the code herein, although having the same foundations as
+#               the Betteredge code, aren't copied & pasted from the Betteredge
+#               code. Moreover, wheresoever appropriate, individual test
+#               routines i.e. test.*(), mirror those made available via the Perl
+#               Test::More module.
+###############################################O################################
+
+eval ${__LIB_SH__:-}
+export __LIB_SH__=return
+
+. ${BASH_SOURCE%.sh}/sinclude.sh
+. console path
+
+#### END OF FILE
